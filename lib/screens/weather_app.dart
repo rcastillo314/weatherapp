05f8af9365cf70db4925b278a6eed46dc3c97ed6
rcastillo619 <leon_619@outlook.com>
2021-03-04 21:17:51 -0800

    change app bar color to transparent

--- lib/screens/weather_app.dart
+++ lib/screens/weather_app.dart
@@ -6,7 +6,9 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         title: Text('Weather App'),
         leading: IconButton(
           icon: Icon(
@@ -17,10 +19,12 @@ class WeatherApp extends StatelessWidget {
         ),
         actions: [
           Container(
+            margin: EdgeInsets.fromLTRB(0.0, 0.0, 20.0, 0.0),
             child: SvgPicture.asset(
               'assets/menu.svg',
               height: 30.0,
               width: 30.0,
+              color: Colors.white,
             ),
           )
         ],
