ef33951e667c37c8c9339edfed939ecbb59918d8
rcastillo619 <leon_619@outlook.com>
2021-03-05 00:25:56 -0800

    add current page state

--- lib/screens/weather_app.dart
+++ lib/screens/weather_app.dart
@@ -4,7 +4,21 @@ import 'package:weatherapp/models/weather_locations.dart';
 import 'package:weatherapp/widgets/single_widget.dart';
 import 'package:weatherapp/widgets/slider_dot.dart';
 
-class WeatherApp extends StatelessWidget {
+class WeatherApp extends StatefulWidget {
+  @override
+  _WeatherAppState createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends State<WeatherApp> {
+  int _currentPage = 0;
+  String backgroundImage;
+
+  _onPageChange(int index) {
+    setState(() {
+      _currentPage = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,12 +68,14 @@ class WeatherApp extends StatelessWidget {
               margin: EdgeInsets.only(top: 140.0, left: 15.0),
               child: Row(
                 children: [
-                  for (int i = 0; i < locationList.length; i++) SliderDot(true)
+                  for (int i = 0; i < locationList.length; i++)
+                    if (i == _currentPage) SliderDot(true) else SliderDot(false)
                 ],
               ),
             ),
             PageView.builder(
               scrollDirection: Axis.horizontal,
+              onPageChanged: _onPageChange,
               itemCount: locationList.length,
               itemBuilder: (ctx, i) => SingleWidget(i),
             ),
