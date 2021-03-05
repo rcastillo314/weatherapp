b73845a6dee0c6c1113f6b3224ef65dccbb873e7
rcastillo619 <leon_619@outlook.com>
2021-03-04 22:45:50 -0800

    add footer layout and mock data

--- lib/screens/weather_app.dart
+++ lib/screens/weather_app.dart
@@ -128,30 +128,133 @@ class WeatherApp extends StatelessWidget {
                         decoration: BoxDecoration(
                             border: Border.all(color: Colors.white)),
                       ),
-                      Row(
-                        children: [
-                          Text(
-                            'Kolkata',
-                            style: GoogleFonts.lato(
-                              fontSize: 22.0,
-                              color: Colors.white,
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  'Wind',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '10',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  'km/h',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Stack(
+                                  children: [
+                                    Container(
+                                      height: 5.0,
+                                      width: 50.0,
+                                      color: Colors.white38,
+                                    ),
+                                    Container(
+                                      height: 5.0,
+                                      width: 5.0,
+                                      color: Colors.greenAccent,
+                                    ),
+                                  ],
+                                )
+                              ],
                             ),
-                          ),
-                          Text(
-                            'Hello',
-                            style: GoogleFonts.lato(
-                              fontSize: 22.0,
-                              color: Colors.white,
+                            Column(
+                              children: [
+                                Text(
+                                  'Rain',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '2',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '%',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Stack(
+                                  children: [
+                                    Container(
+                                      height: 5.0,
+                                      width: 50.0,
+                                      color: Colors.white38,
+                                    ),
+                                    Container(
+                                      height: 5.0,
+                                      width: 5.0,
+                                      color: Colors.redAccent,
+                                    ),
+                                  ],
+                                )
+                              ],
                             ),
-                          ),
-                          Text(
-                            'Kolkata',
-                            style: GoogleFonts.lato(
-                              fontSize: 22.0,
-                              color: Colors.white,
+                            Column(
+                              children: [
+                                Text(
+                                  'Humidity',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '10',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  '%',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Stack(
+                                  children: [
+                                    Container(
+                                      height: 5.0,
+                                      width: 50.0,
+                                      color: Colors.white38,
+                                    ),
+                                    Container(
+                                      height: 5.0,
+                                      width: 5.0,
+                                      color: Colors.redAccent,
+                                    ),
+                                  ],
+                                )
+                              ],
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       )
                     ],
                   ),
